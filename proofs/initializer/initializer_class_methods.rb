1c20d5e3e1d62f4ebdfbfc53a0a94242fe4cbb92
Scott Bellware <sbellware@gmail.com>
2013-01-31 19:07:59 -0700

    Class methods are tested directly (as dsl)

--- proofs/initializer/initializer_class_methods.rb
+++ proofs/initializer/initializer_class_methods.rb
@@ -1,61 +1,60 @@
 require_relative '../proofs_init'
 
-title 'Initializer Class Methods'
+title 'Initializer DSL'
 
-module InitializerClassMethods
-  class SomeClass
-    include ::Initializer::ClassMethods
-    include ::Proof
-  end
-end
-
-module InitializerClassMethods
-  class SomeClass
+module Initializer
+  class ParameterConfig
     module Proof
-      def creates_read_parameter_configuration?(name)
-        param = reader name
-        param.extension_module == Initializer::ReaderParameter
+      def named?(name)
+        parameter_name == name
       end
-      def creates_no_accessor_parameter_configuration?(name)
-        param = no_accessor name
-        param.extension_module == Initializer::NoAccessorParameter
+      def extension?(mod)
+        extension_module == mod
       end
-      def creates_write_parameter_configuration?(name)
-        param = writer name
-        param.extension_module == Initializer::WriterParameter
+      def reader_parameter?
+        extension? Initializer::ReaderParameter
       end
-      def creates_accessor_parameter_configuration?(name)
-        param = accessor name
-        param.extension_module == Initializer::AccessorParameter
+      def writer_parameter?
+        extension? Initializer::WriterParameter
+      end
+      def accessor_parameter?
+        extension? Initializer::AccessorParameter
+      end
+      def no_accessor_parameter?
+        extension? Initializer::NoAccessorParameter
       end
     end
   end
 end
 
-def class_methods
-  InitializerClassMethods::SomeClass.new
+def dsl
+  ::Initializer::ClassMethods
 end
 
 heading 'Reader macro style method' do
   proof 'Creates a ParameterConfig with the specified name and the ReaderParameter module as its mixin' do
-    class_methods.prove { creates_read_parameter_configuration? :age }
+    config = dsl.reader(:age)
+    config.prove { reader_parameter? && named?(:age) }
   end
 end
 
 heading 'Writer macro style method' do
   proof 'Creates a ParameterConfig with the specified name and the WriterParameter module as its mixin' do
-    class_methods.prove { creates_write_parameter_configuration? :age }
+    config = dsl.writer(:age)
+    config.prove { writer_parameter? && named?(:age) }
   end
 end
-
+# NOTE solve issue with send
 heading 'Accessor macro style method' do
   proof 'Creates a ParameterConfig with the specified name and the AttrParameter module as its mixin' do
-    class_methods.prove { creates_accessor_parameter_configuration? :age }
+    config = dsl.accessor(:age)
+    config.prove { accessor_parameter? && named?(:age) }
   end
 end
 
 heading 'No accessor macro style method' do
   proof 'Creates a ParameterConfig with the specified name and the ParameterWithNoAccessors module as its mixin' do
-    class_methods.prove { creates_no_accessor_parameter_configuration? :age }
+    config = dsl.no_accessor(:age)
+    config.prove { no_accessor_parameter? && named?(:age) }
   end
 end
