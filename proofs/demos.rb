9dbbd26779d0bd368fe0299f41fc622e8eb851c8
Scott Bellware <sbellware@gmail.com>
2013-02-06 20:47:10 -0700

    File loaders are no longer necessary given the suite

--- proofs/demos.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'proofs_init'
-
-title "Demos"
-
-load 'demos/initializer_with_default_visibility.rb'
-load 'demos/initializer_with_parameter_visibility.rb'
-load 'demos/initializer_without_parameter_visibility.rb'
--- proofs/suite.rb
+++ proofs/suite.rb
@@ -1,4 +1,3 @@
 require_relative 'proofs_init'
 
 Proof::Runner::Suite.run 'initializer/**/*.rb', 'demos/**/*.rb'
-
